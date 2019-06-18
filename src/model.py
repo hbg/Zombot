f8dbc4aaca34e0d0e12cfffccd1673f38d5cc8bc
Harris Beg <legostarwarsguy02@gmail.com>
2019-06-17 19:07:42 -0700

    Cleanup

--- src/model.py
+++ src/model.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 import json
 import time
-DRIVER = 'C:\Python27\selenium\webdriver\chromedriver.exe'
+DRIVER = r'C:\Python27\selenium\webdriver\chromedriver.exe'
 HEADER = {'User-Agent': 'Mozilla/5.0'}
 browser = webdriver.Chrome(DRIVER)
 # URL = "https://api.hackertarget.com/pagelinks/?q="
@@ -14,19 +14,23 @@ class Model(object):
     def __init__(self):
         self.master = {}
         self.current_url = ""
-    def get_response(self, site):
+
+    @staticmethod
+    def get_response(site):
         links = []
         req = Request(site, headers=HEADER)
         resp = urlopen(req)
         html = BeautifulSoup(resp)
         for href in html.find_all('a', href=True):
-            if href['href'][0:1] == '/':
+            if href['href'][0:1] == '/' or href['href'][0:1] == '#':
+                # Using slices is a bit weird, but it may come in handy
                 links.append(browser.current_url + href['href'])
             else:
                 links.append(href['href'])
         return links
 
-    def emulate(self):
+    @staticmethod
+    def emulate() -> list:
         path = []
         model = open("model.json").read()
         markov_values = json.loads(model)
@@ -36,16 +40,13 @@ class Model(object):
             for pair in value:
                 e.append(pair[0])
                 p.append(pair[1])
-            sample = np.random.choice(e, 1, p)[0]
+            sample = np.random.choice(e, 1, p=p)[0]
+            # Not a true Markov Chain since the next sample's URL doesn't depend on the previous sample
             path.append(sample)
         for website in path:
-            try:
-                browser.get(website)
-                time.sleep(1)
-            except:
-                print("URL {} doesn't work".format(website))
-
-
+            browser.get(website)
+            time.sleep(1)
+        return path
 
     def train(self):
         self.current_url = browser.current_url
@@ -57,7 +58,8 @@ class Model(object):
                     for line in results:
                         if self.master.get(self.current_url):
                             if browser.current_url in results:
-                                self.master[self.current_url].append((line, (count-int(count/2))/(count*(count-1)))) if line != browser.current_url else self.master[self.current_url].append((line, int(count/2)/count))
+                                self.master[self.current_url].append(
+                                    [line, (count - int(count / 2)) / (count * (count - 1))]) if line != browser.current_url else self.master[self.current_url].append((line, int(count / 2) / count))
                             else:
                                 self.master[self.current_url].append((line, 1/count))
                         else:
